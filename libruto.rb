94c5a9f46accf1f2f4ed61c9462a48a6f459204c
Bryan Mikaelian <bryan@librato.com>
2016-09-14 16:02:56 -0500

    Bump version

--- libruto.rb
+++ libruto.rb
@@ -5,10 +5,11 @@ $statsd = Statsd.new '127.0.0.1', 8125
 
 class Libruto < Sinatra::Base
   get '/' do
-    $statsd.increment 'bryan.tests', 1, tags: { application: 'sinatra', ruby: '2.4.1.beta' }
-    $statsd.increment 'bryan.tests', 1, tags: { application: 'sinatra', ruby: '2.3.3' }
-    $statsd.increment 'bryan.tests', 1, tags: { application: 'sinatra', ruby: '1.9.3' }
-    $statsd.increment 'bryan.tests', 1, tags: { application: 'sinatra', ruby: '2.2.3' }
+    $statsd.increment 'bryan.tests', tags: { application: 'sinatra', ruby: '2.4.1.beta' }
+    $statsd.increment 'bryan.testws', tags: { application: 'sinatra', ruby: '2.3.3' }
+    $statsd.increment 'bryan.tests', tags: { application: 'sinatra', ruby: '1.9.3' }
+    $statsd.increment 'bryan.tests', tags: { application: 'sinatra', ruby: '2.2.3' }
+    $statsd.gauge'bryan.tests', Random.rand(100), tags: { application: 'sinatra', action: 'index' }
     'Libruto'
   end
 end
